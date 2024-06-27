997d806ce3caab287d0ef7fca124a2439f437b9c
ParkAsher <devasherpark@naver.com>
2024-06-27 17:33:19 +0900

    [chore]: API 통신관련 패키지 설치

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <Header />
             <Routes>
                 <Route path='/login' element={<Login />} />
                 <Route path='/signup' element={<Signup />} />
             </Routes>
-        </>
+        </QueryClientProvider>
     );
 }
 
