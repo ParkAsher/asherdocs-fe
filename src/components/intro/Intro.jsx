bdce6548af6b52c3c55809c0fcff13d82d5eba2c
ParkAsher <devasherpark@naver.com>
2024-10-22 15:18:48 +0900

    [design]: 소개 컴포넌트 디자인 수정

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,18 +5,21 @@ import { IoMdMail } from 'react-icons/io';
 
 function Intro() {
     return (
-        <div className='w-full my-10'>
-            <div className='w-full flex items-center justify-center flex-wrap gap-5 h-full mx-auto my-0 p-10 border-solid border-b border-gray-300 lg:w-[800px]'>
+        <div className='w-full border-b border-solid border-gray-300 py-10'>
+            <div className='w-full flex items-center justify-center flex-wrap gap-5'>
                 <div className='h-40 w-40'>
                     <img className='w-full h-full' src={logoImage} />
                 </div>
-                <div className='flex items-center flex-wrap gap-3'>
-                    <a className='w-10 h-10' href='https://github.com/ParkAsher'>
-                        <FaGithub className='w-full h-full' />
-                    </a>
-                    <a className='w-10 h-10' href='mailto:devasherpark@gmail.com'>
-                        <IoMdMail className='w-full h-full' />
-                    </a>
+                <div className='flex items-center flex-wrap flex-col gap-5'>
+                    <div className='text-xl font-semibold'>백엔드 개발자 박현민</div>
+                    <div className='flex items-center flex-wrap gap-3'>
+                        <a className='w-10 h-10' href='https://github.com/ParkAsher'>
+                            <FaGithub className='w-full h-full' />
+                        </a>
+                        <a className='w-10 h-10' href='mailto:devasherpark@gmail.com'>
+                            <IoMdMail className='w-full h-full' />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
