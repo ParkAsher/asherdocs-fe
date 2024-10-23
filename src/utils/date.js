68ce37f135e35ac9dbf27fc423a0d9ba12aa04bb
ParkAsher <devasherpark@naver.com>
2024-10-23 15:50:05 +0900

    [fix]: 날짜 포매팅 수정

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,6 @@
-import { format } from 'date-fns';
-
 export const formatDate = (date) => {
-    return format(date, 'yyyy.MM.dd HH:mm:ss');
+    const [datePart, timePart] = date.split('T'); // 'T'로 나누기
+    const time = timePart.split('.')[0]; // 밀리초 제거
+
+    return `${datePart.replace(/-/g, '.')} ${time}`; // yyyy.MM.dd HH:mm:ss 형식으로 변환
 };
