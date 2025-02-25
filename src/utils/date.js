f7794e7bf08b96704a49dd5501869f2cace49a2d
ParkAsher <devasherpark@naver.com>
2025-02-25 15:13:20 +0900

    [chore]: date 관련 패키지 설치 및 날짜 포맷 유틸 함수 생성

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,25 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import { ko } from 'date-fns/locale';
+
 export const formatDate = (date) => {
     const [datePart, timePart] = date.split('T'); // 'T'로 나누기
     const time = timePart.split('.')[0]; // 밀리초 제거
 
     return `${datePart.replace(/-/g, '.')} ${time}`; // yyyy.MM.dd HH:mm:ss 형식으로 변환
 };
+
+export function notificationTimeFormat(date) {
+    const targetDate = new Date(date);
+    const now = new Date();
+    const diff = now.getTime() - targetDate.getTime();
+
+    if (diff < 1000 * 60 * 1) {
+        return '방금 전';
+    }
+
+    if (diff < 1000 * 60 * 60 * 24 * 7) {
+        return formatDistanceToNow(targetDate, { addSuffix: true, locale: ko });
+    }
+
+    return format(targetDate, 'yyyy년 MM월 dd일', { locale: ko });
+}
